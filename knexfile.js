b7f0430a4005cbc62491947f087427d3076b79c1
Jason Clifton <j.clifton1473@gmail.com>
2017-07-25 10:51:33 -0600

    added production connection

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,10 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/express-warmup'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 
 };
